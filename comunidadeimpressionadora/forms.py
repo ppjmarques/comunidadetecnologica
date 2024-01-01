695335a84f54496ddef25fa930a4bcb6cfd2b238
ppjmarques <151474327+ppjmarques@users.noreply.github.com>
2024-01-01 16:29:55 -0400

    Add files via upload

---
+++ comunidadeimpressionadora/forms.py
@@ -0,0 +1,53 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from comunidadeimpressionadora.models import Usuario
+from flask_login import current_user
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
+
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png'])])
+    tecnologia_logistica = BooleanField('Logística')
+    tecnologia_manufatura = BooleanField('Manufatura')
+    tecnologia_engenharia = BooleanField('Engenharia')
+    tecnologia_ciencia_dados = BooleanField('Ciência de Dados')
+    tecnologia_ciencia_computacao = BooleanField('Ciência da Computação')
+    tecnologia_engenharia_software = BooleanField('Engenharia de Software')
+    tecnologia_inteligencia_artificial = BooleanField('Inteligência Artificial')
+    tecnologia_marketing_digital = BooleanField('Marketing Digital')
+    botao_submit_editarperfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com esse e-mail. Cadastre outro e-mail')
+
+
+class FormCriarPost(FlaskForm):
+    titulo = StringField('Título do Post', validators=[DataRequired(), Length(2, 140)])
+    corpo = TextAreaField('Escreva seu Post Aqui', validators=[DataRequired()])
+    botao_submit = SubmitField('Criar Post')
---
+++ comunidadeimpressionadora/teste.py
@@ -0,0 +1,4 @@
+from comunidadeimpressionadora import app, database
+from comunidadeimpressionadora.models import Usuario, Post
+
+database.create_all()
