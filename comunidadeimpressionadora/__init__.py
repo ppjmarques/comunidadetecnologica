6a9867e194a6273fcc87cfb5051992e17f9c55b7
ppjmarques <151474327+ppjmarques@users.noreply.github.com>
2024-01-01 17:25:20 -0400

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 
 if os.getenv("DATABASE_URL"):
@@ -13,13 +13,20 @@ if os.getenv("DATABASE_URL"):
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
-
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados criada")
+else:
+    print('Base de Dados já existente')
 from comunidadeimpressionadora import routes
-
